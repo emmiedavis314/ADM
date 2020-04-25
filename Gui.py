90be40adbab0c369802db828f4a7089001a85ef2
natalie1998 <52354442+natalie1998@users.noreply.github.com>
2020-04-24 20:11:53 -0500

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -1,59 +1,93 @@
 # Class that contains the GUI for the game
 
 #import
+import sys
 from tkinter import *
 from tkinter.ttk import *
-
+from beginning import *
 #root that creates the main window, has to be first
 
-MainWindow = Tk()
-
-#define functions
-def header():
-    h = Frame(MainWindow)
-    title = Label(h, text="Welcome to Aggie Decision Maker!")
-    title.pack()
-    subtitle = Label(h, text="This is the subtitle")
-    subtitle.pack()
-    button = Button(h, text="Test")
-    button.pack()
-    h.pack()
-
-def healthbars(health, gpa, social):
-    progress = Frame(MainWindow)
-    he = Frame(progress)
-    so = Frame(progress)
-    gp = Frame(progress)
-    h = Progressbar(he, orient=HORIZONTAL, length=100, mode='determinate')
-    s = Progressbar(so, orient=HORIZONTAL, length=100, mode='determinate')
-    g = Progressbar(gp, orient=HORIZONTAL, length=100, mode='determinate')
-    h['value'] = health
-    s['value'] = gpa
-    g['value'] = social
-    h_text = Label(he, text="Health: ")
-    h_text.pack(side=LEFT)
-    s_text = Label(so, text="Social:  ")
-    s_text.pack(side=LEFT)
-    g_text = Label(gp, text="GPA:     ")
-    g_text.pack(side=LEFT)
-    h.pack(side=RIGHT)
-    s.pack(side=RIGHT)
-    g.pack(side=RIGHT)
-    he.pack()
-    so.pack()
-    gp.pack()
-    progress.pack()
 
 
-def footer():
-    b = Frame(MainWindow)
-    foot = Label(b, text="Created for CSCE 445 @ TAMU by Natalie Burks, Emily Davis, Allison Reuthinger")
-    foot.pack()
-    b.pack(side=BOTTOM)
+class Menu():
+    m = globals()['Begin']
+    s_bar = 100
+    h_bar = 100
+    g_bar = 100
+    functionName = "Hello"
 
-#create widgets
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+        title = Label(self.master, text="Welcome to Aggie Decision Maker!")
+        self.b1 = Button(self.master, text="Begin",command=lambda:[self.b1.pack_forget(), self.newWindow(), title.pack_forget()])
+        foot = Label(self.master, text="Created for CSCE 445 @ TAMU by Natalie Burks, Emily Davis, Allison Reuthinger")
+        foot.pack()
+        foot.pack(side=BOTTOM)
+        title.pack()
+        self.b1.pack()
+        self.frame.pack()
 
-header()
-healthbars(100, 70, 50)
-footer()
-MainWindow.mainloop()
+    def update_health(self, h, s, g):
+        Menu.h_bar -= h
+        Menu.s_bar -= s
+        Menu.g_bar -= g
+        #self.healthbars(Menu.h_bar, Menu.s_bar, Menu.g_bar)
+        #self.newWindow()
+
+    def healthbars(self, s_bar, g_bar, h_bar):
+        progress = Frame(self.frame)
+        he = Frame(progress)
+        so = Frame(progress)
+        gp = Frame(progress)
+        h = Progressbar(he, orient=HORIZONTAL, length=s_bar, mode='determinate')
+        s = Progressbar(so, orient=HORIZONTAL, length=g_bar, mode='determinate')
+        g = Progressbar(gp, orient=HORIZONTAL, length=h_bar, mode='determinate')
+        h['value'] = Menu.h_bar
+        s['value'] = Menu.s_bar
+        g['value'] = Menu.g_bar
+        h_text = Label(he, text="Health: ")
+        h_text.pack(side=LEFT)
+        s_text = Label(so, text="Social:  ")
+        s_text.pack(side=LEFT)
+        g_text = Label(gp, text="GPA:     ")
+        g_text.pack(side=LEFT)
+        h.pack(side=RIGHT)
+        s.pack(side=RIGHT)
+        g.pack(side=RIGHT)
+        he.pack()
+        so.pack()
+        gp.pack()
+        progress.pack()
+
+    def newWindow(self):
+        self.frame = Frame(self.master)
+        title = Label(self.frame, text='Question 1')
+        title.pack()
+        self.healthbars(Menu.s_bar, Menu.g_bar, Menu.h_bar)
+        self.frame.pack()
+        Begin.bQuest1(self)
+
+    def newWindow2(self, functionName, Q):
+        self.frame = Frame(self.master)
+        title = Label(self.frame, text=Q)
+        title.pack()
+        self.healthbars(Menu.s_bar, Menu.g_bar, Menu.h_bar)
+        self.frame.pack()
+        func = getattr(Menu.m, functionName)(self)
+
+class mainGame():
+
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+        self.b3 = Button(self.master,text="text")
+
+
+
+    #create widgets
+
+    #header()
+    #healthbars(100, 70, 50)
+    #footer()
+    #MainWindow.mainloop()
